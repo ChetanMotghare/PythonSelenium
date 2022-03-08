20b0aec028ddeb15871e1489fda32ef38fcb3bd7
chetan <chetan.motghare22@gmail.com>
2022-03-08 09:55:35 +0530

    8/03/22--

--- Basic/OOPS/Variable.py
+++ Basic/OOPS/Variable.py
@@ -1,11 +1,24 @@
+
 a,b=40,45
 class CheckVariables:
-    a=90
-    b=60
+    a,b=90,50
     def add(self,a,b):
-        print("Addition of local var :: ",a+b)
-        print("Addition of class var :: ",self.a+self.b)
-        print("Addition of global var :: ",globals()['a']+globals()['b'])
+        print(a+b)
+        print(self.a+self.b)
+        print(globals()['a']+globals()['b'])
 
-ckl=CheckVariables
-ckl.add(78,40,76)
+ckl=CheckVariables()
+ckl.add(200,100)
+
+#constructor
+class Constructor:
+    def __init__(self):
+        print("Constructor block")
+    def method(self,name):
+        print(name)
+    def addition(self,a,b):
+        print(a+b)
+cons = Constructor()
+cons.method("Chetan")
+cons.addition(100,200)
+
